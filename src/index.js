33e2a797eea55a9049e124b02650bf20926c94a3
Tyler McCormick <tyler.mccormick@colorado.edu>
2022-08-01 21:25:00 -0600

    add footer

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <footer>Created by <a href="https://typicel.me">typicel</a> | <a href="https://github.com/typicel">View on Github</a></footer>
   </React.StrictMode>
 );
